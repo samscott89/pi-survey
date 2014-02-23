650fd9b808ca5b314d69dd95858fd86054f55df3
Sam Scott <sam.scott89@gmail.com>
2014-02-23 16:08:16 +0000

    Changed secret token to generated version

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SurveyApp::Application.config.secret_key_base = '9dfde388361aa4f96bd7895ba0d3046fa62225d8ee01117d86b05a376d68d98736c84c99568cc0d5ee86b60aca5cc54162bc4e697cc5e7019a824c93a020122a'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
