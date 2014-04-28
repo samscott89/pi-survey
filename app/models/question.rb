13ea76b29dccffa03ff6c847c8ec23f46a68c1bb
Sam Scott <sam.scott89@gmail.com>
2014-04-28 13:39:13 +0100

    Created images for the project outline (not for use in the code, but simply as a guide).
    Also made a minor edit to the comments in question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 
 	belongs_to :survey_section, dependent: :destroy
-	belongs_to :option_group, foreign_key: "group_id" #This isn't strictly necessary, but might make things easier later.
+	belongs_to :option_group, foreign_key: "group_id"
 	has_many :question_options
 	has_many :option_choices, through: :question_options
 
