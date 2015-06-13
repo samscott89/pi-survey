cc7ef90a726620b3c07ad2ba29702ee4a1538cbd
Sam Scott <sam.scott89@gmail.com>
2015-06-13 01:11:44 +0100

    Few more minor authorisation changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 #  before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
+  skip_authorization_check
+
   def show
     @user = current_user
 
