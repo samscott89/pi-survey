352fa3ea674188bb4137eecd97439111321bcb97
Sam Scott <sam.scott89@gmail.com>
2015-07-07 23:08:54 -0700

    A few suggestions for some changes.
    If you add .erb to surveys.js, then it can be preprocessed by Ruby.
    Therefore, we can use the multiples scope to get all the relevant QuestionType ids.

--- app/models/option_group.rb
+++ app/models/option_group.rb
@@ -11,17 +11,4 @@ class OptionGroup < ActiveRecord::Base
 	belongs_to :question_type, foreign_key: "type_id"
 	
 	validates :type_id, presence: true
-
-	@@multiple_types = [1,2,5,6]
-
-	/
-	Helper function to determine if OptionGroup expects multiple responses
-	/
-	def multiple?
-		@@multiple_types.include? self.type_id
-	end 
-
-	def self.multiples
-		OptionGroup.where(type_id: @@multiple_types).ids
-	end
 end
