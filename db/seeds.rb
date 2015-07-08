2c1635b000041c507be736847196f3be12765472
Sam Scott <sam.scott89@gmail.com>
2015-07-07 22:49:19 -0700

    Added the admin user creator back in.
    Also provided a secrets.yml.template to fix the error you'll get if
    secrets.yml is missing/incomplete.
    
    Use the template, with your own version of the admin password to make it work.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#user = CreateAdminService.new.call
-#puts 'Created Admin User: ' << user.email
+user = CreateAdminService.new.call
+puts 'Created Admin User: ' << user.email
 
 
 guest = User.find_by(email: "guest@example.com")
