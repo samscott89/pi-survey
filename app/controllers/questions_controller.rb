2ff3565aab1c2862e0a4cf5b96223b2991d471c3
Sam Scott <sam.scott89@gmail.com>
2015-07-26 11:45:10 -0700

    Clean up of questions controller.
    TODO: Needs to handle deleting of multiple choice
    answers on change of type.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -29,47 +29,10 @@ class QuestionsController < ApplicationController
 		
 
 		authorize! :edit, @survey
-
+		was_multiple = @question.question_type.is_multiple?
 		@question.assign_attributes(question_params)
-=begin
-
-	    if @question.option_group.multiple?
-
-	    	#Update all the existing records
-		   	update_option_params.each do |index, opt|
-    		  opt_choice = OptionChoice.find(opt[:id])
-    		  opt_choice.assign_attributes({choice_name: opt[:choice_name]})
- 		      pending_updates << opt_choice
-
-		      if !opt_choice.valid?
-		        @errors << opt_choice.errors
-		        flash[:error] = "There were errors with your answers: #{option_params}"
-		      end
-		    end
-
-		    #Create the new options
-		    option_params.each do |index, opt|
-		      next if opt[:choice_name].empty?
-		      opt_choice = OptionChoice.new(opt.merge(option_group_id: params[:question][:group_id]))
-		      pending_new << opt_choice
-
-		      if !opt_choice.valid?
-		        @errors << opt_choice.errors
-		        flash[:error] = "There were errors with your answers: #{option_params}"
-		      end
-		    end
-		else
-			opt_choice = OptionChoice.new(option_group_id: params[:question][:group_id], choice_name: "other")
-			pending_new << opt_choice
-			if !opt_choice.valid?
-		      @errors << opt_choice.errors
-		      flash[:error] = "There were errors with your answers: #{option_params}"
-		    end
-		end
-=end
 
 		if !@question.valid?
-		  1/0
 		  # render 'show'
 		  flash[:error] = "Errors with updating question."
 		  redirect_to :back
@@ -95,19 +58,6 @@ class QuestionsController < ApplicationController
 	  	  	params.require(:question).permit(:name, :subtext, :required, :allow_other,
 	  	  		option_group_attributes: [:type_id, 
 	  	  			option_choices_attributes: [:choice_name, :_destroy]])
-	  	  	# params.require(:question).permit(:subtext, :required, option_group: [:question_type, choice_name: []])
-	  	  	# params.require(:question).permit(:name, :subtext, :required, :group_id, :allow_other)
 	  	  end
 
-	  	  def option_params
-	  	  	params.require(:option_choice)
-	  	  end
-
-	  	  def update_option_params
-	  	  	if params[:question].has_key? :option_choices_attributes
-		  	  	params.require(:question).require(:option_choices_attributes) 
-		  	else
-		  		[]
-		  	end
-	  	  end
 end
