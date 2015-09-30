5f8045f0cd8442c3d3e38a75733e2d10d98c7812
Sam Scott <sam.scott89@gmail.com>
2015-09-30 23:41:05 +0100

    Few minor fix attempts
    Added bootstrap-sprockets to enable glyphicons.
    Changed the assets version to get them to recompile.
    Added *.js into precompiled files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ SurveyApp::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '0.1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -60,7 +60,7 @@ SurveyApp::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( *.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
